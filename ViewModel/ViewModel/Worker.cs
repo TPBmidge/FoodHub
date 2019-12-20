1593cfe55d96bb91bf2f7b8a0615b23acf2d7937
buerger <buerger@MB>
2019-12-20 10:33:10 +0100

    Forgot Password hinzugefügt

---
+++ ViewModel/ViewModel/Worker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodHub.ViewModel
+{
+    public partial class ViewModel
+    {
+        static string cn_String = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\buerger\Source\Repos\TPBmidge\FoodHub\View\ViewCore\dbFoodHub.mdf;Integrated Security = True";
+        public static void Worker()
+        {
+
+            SqlConnection cn = new SqlConnection(cn_String);
+
+        }
+    }
+}
